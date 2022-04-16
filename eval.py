701cbb08ae8588bc9903509ae440a8fac5fbae3b
Zhaoning Li (李肇宁) <yc17319@umac.mo>
2022-04-16 22:25:41 +0800

    dra

--- eval.py
+++ eval.py
@@ -8,6 +8,8 @@ from statsmodels.stats.multitest import multipletests
 from scipy.stats import kstest, wilcoxon
 from pandas.core.frame import DataFrame
 from scipy.stats import zscore
+from pymer4.models import Lmer
+import pandas as pd
 
 SS_mvs, SO_mvs, OS_mvs = load_IMQ_data(score='mvs')
 SS_uvs, SO_uvs, OS_uvs = load_IMQ_data(score='uvs')
@@ -473,3 +475,337 @@ def wcx_test(data):
     pvals.append(pval_12); pvals.append(pval_13); pvals.append(pval_14); pvals.append(pval_23); pvals.append(pval_24); pvals.append(pval_34)
         
     return diffs, pvals
+
+
+def model_selection(results, combination, parameters, titles, mms_pc=None, show=False):
+    
+    """
+    Select the optimal parameter configuration with the the highest average IDM correspondence from subject-wise bootstrapping
+    
+    Parameters
+    ----------
+    results: list.
+        The subject-wise bootstrapping results grouped by the same pipeline.
+    combination: list.
+        The list of tuples, which consists of the combination of modalities.
+    parameters: list.
+        The list of tuples, which consists of the combination of parameters.
+    titles: list.
+        The list of tuples, which consists of the combination of names of modalities.
+    mms_pc: string 'NPP' or 'PP' or 'PPw' or 'CPP-SD'. Default is None.
+        The pipeline to construct inter-subject MMS dissimilarity matrix.
+        'NPP' for without patching and pooling operations and computing surface distance,
+        'PP' or 'PPw' (wmd and wrd) for with patching and pooling operations but without computing surface distance,
+        'CPP-SD' for computing patching and pooling operations-based surface distance.
+    bsm: bool True or False. Default is False.
+        If bsm is True, then only return the highest average IDM correspondence
+        If bsm is False, then return the info about the optimal parameter configuration
+    show: bool True or False. Default is False.
+        Print info or not.
+    
+    Returns
+    -------
+    winner: list. 
+        The info about the optimal parameter configuration
+    pvals: list. 
+        The p-value about the optimal parameter configuration
+    bs_mean: list. 
+        The highest average IDM correspondence
+    """
+
+    winner, pvals, bs_mean = [], [], []
+    num_p = len(parameters)
+
+    for i, r in tqdm(enumerate(results)):
+
+        idx, m = sorted([p for p in enumerate(mean(r, num_p))],
+                        key=lambda x: x[-1],
+                        reverse=True)[0]
+        
+        bs_mean.append((m, r[idx][-1]))
+            
+        x = combination[i][0]
+                
+        if 'SS' in titles[i] or 'SO' in titles[i] or 'OS' in titles[i]:
+            
+            if parameters[idx][0] in DSF_X_mvs:
+                x = combination[i][0][0]
+                
+            if parameters[idx][0] in DSF_X_uvs:
+                x = combination[i][0][1]
+            
+        if mms_pc == 'NPP':
+            
+            rho, pv = run(x,
+                          combination[i][1],
+                          parameters[idx][0],
+                          parameters[idx][1],
+                          iter=10000)
+
+            if show:
+                print(titles[i] + ': ' + 'rho ' + '%.4f' % float(rho) + ' p ' +
+                      '%.4f' % float(pv) + ' achieved by ' + parameters[idx][0] +
+                      '-' + parameters[idx][1] + ' ' + '%.4f' % float(m))
+
+        else:
+            rho, pv = run(x,
+                          combination[i][1],
+                          parameters[idx][0],
+                          parameters[idx][1],
+                          parameters[idx][2],
+                          iter=10000)
+                
+            if show:
+                print(titles[i] + ': ' + 'rho ' + '%.4f' % float(rho) + ' p ' +
+                      '%.4f' % float(pv) + ' achieved by ' + parameters[idx][0] +
+                      '-' + parameters[idx][1] + '-' +
+                      str(parameters[idx][2][1][0]) + '-' +
+                      str(parameters[idx][2][2]) + ' ' + '%.4f' % float(m))
+
+        winner.append((r[idx], rho, m, parameters[idx]))
+        pvals.append(pv)
+            
+    return winner, pvals, bs_mean
+
+
+def eval_all(combination, w_npp, w_pp, w_ppw, w_cpp_sd, pv_npp, pv_pp, pv_ppw, pv_cpp_sd,
+             titles):
+    
+    """
+    Evaluate all the average IDM correspondence from bootstrapping for different pipelines
+    """
+
+    pvals_all = []
+
+    for i in range(len(combination)):
+
+        idx, m = sorted([(0, w_npp[i][2]), (1, w_pp[i][2]), (2, w_ppw[i][2]),
+                         (3, w_cpp_sd[i][2])],
+                        key=lambda x: x[-1],
+                        reverse=True)[0]
+
+        if idx == 0:
+            rho = w_npp[i][1]
+            pv = pv_npp[i]
+            parameters = w_npp[i][-1]
+            m = w_npp[i][2]
+            l, h = st.norm.interval(alpha=0.95,
+                                    loc=np.mean(w_npp[i][0][1]),
+                                    scale=st.sem(w_npp[i][0][1]))
+            print('NPP:    ' + titles[i] + ': ' + 'rho ' + '%.4f' % float(rho) +
+                  ' p ' + '%.4f' % float(pv) + ' mean ' + '%.4f' % float(m) +
+                  ' CI ' + '%.4f' % float(l) + '-' + '%.4f' % float(h) +
+                  ' achieved by ' + parameters[0] + '-' + parameters[1])
+
+        elif idx == 1:
+            rho = w_pp[i][1]
+            pv = pv_pp[i]
+            parameters = w_pp[i][-1]
+            m = w_pp[i][2]
+            l, h = st.norm.interval(alpha=0.95,
+                                    loc=np.mean(w_pp[i][0][1]),
+                                    scale=st.sem(w_pp[i][0][1]))
+            print('PP:     ' + titles[i] + ': ' + 'rho ' + '%.4f' % float(rho) +
+                  ' p ' + '%.4f' % float(pv) + ' mean ' + '%.4f' % float(m) +
+                  ' CI ' + '%.4f' % float(l) + '-' + '%.4f' % float(h) +
+                  ' achieved by ' + parameters[0] + '-' + parameters[1] + '-' +
+                  str(parameters[2][1][0]) + '-' + str(parameters[2][2]))
+
+        elif idx == 2:
+            rho = w_ppw[i][1]
+            pv = pv_ppw[i]
+            parameters = w_ppw[i][-1]
+            m = w_ppw[i][2]
+            l, h = st.norm.interval(alpha=0.95,
+                                    loc=np.mean(w_ppw[i][0][1]),
+                                    scale=st.sem(w_ppw[i][0][1]))
+            print('PPw:    ' + titles[i] + ': ' + 'rho ' + '%.4f' % float(rho) +
+                  ' p ' + '%.4f' % float(pv) + ' mean ' + '%.4f' % float(m) +
+                  ' CI ' + '%.4f' % float(l) + '-' + '%.4f' % float(h) +
+                  ' achieved by ' + parameters[0] + '-' + parameters[1] + '-' +
+                  str(parameters[2][1][0]) + '-' + str(parameters[2][2]))
+
+        elif idx == 3:
+            rho = w_cpp_sd[i][1]
+            pv = pv_cpp_sd[i]
+            parameters = w_cpp_sd[i][-1]
+            m = w_cpp_sd[i][2]
+            l, h = st.norm.interval(alpha=0.95,
+                                    loc=np.mean(w_cpp_sd[i][0][1]),
+                                    scale=st.sem(w_cpp_sd[i][0][1]))
+            print('CPP-SD: ' + titles[i] + ': ' + 'rho ' + '%.4f' % float(rho) +
+                  ' p ' + '%.4f' % float(pv) + ' mean ' + '%.4f' % float(m) +
+                  ' CI ' + '%.4f' % float(l) + '-' + '%.4f' % float(h) +
+                  ' achieved by ' + parameters[0] + '-' + parameters[1] + '-' +
+                  str(parameters[2][1][0]) + '-' + str(parameters[2][2]) + '-' + str(parameters[2][3]))
+
+        pvals_all.append(pv)
+
+    return pvals_all
+
+
+def data_prep(results_mms, results_rsfc, target, combination_mms, combination_rsfc, parameters_mms, parameters_rsfc, titles):
+
+    """
+    Prepare the dataset for the following dyadic regression analysis
+    """
+    
+    imq_idms, mms_idms, rsfc_idms = [], [], []
+    num_p_mms = len(parameters_mms)
+    num_p_rsfc = len(parameters_rsfc)
+
+    print('IMQ-MMS')
+
+    for i, r in enumerate(results_mms):
+        
+        r = [(j[0], [-1]) if j[0].split('-')[0] != target else j for j in r]
+
+        idx, m = sorted([p for p in enumerate(mean(r, num_p_mms))],
+                        key=lambda x: x[-1],
+                        reverse=True)[0]
+            
+        if parameters_mms[idx][0] in DSF_X_mvs:
+            x = combination_mms[i][0][0]
+                
+        if parameters_mms[idx][0] in DSF_X_uvs:
+            x = combination_mms[i][0][1]
+            
+        if parameters_mms[idx][-1] == None:            
+            rho, imq_idm, mms_idm = run(x,
+                                        combination_mms[i][1],
+                                        parameters_mms[idx][0],
+                                        parameters_mms[idx][1],
+                                        idm=True)
+
+            print(titles[i] + ': ' + '%.4f' % float(rho) + ' achieved by ' + parameters_mms[idx][0] +
+                  '-' + parameters_mms[idx][1] + ' ' + '%.4f' % float(m))
+
+        else:
+            rho, imq_idm, mms_idm = run(x,
+                                        combination_mms[i][1],
+                                        parameters_mms[idx][0],
+                                        parameters_mms[idx][1],
+                                        parameters_mms[idx][2],
+                                        idm=True)
+
+            print(titles[i] + ': ' + '%.4f' % float(rho) + ' achieved by ' + parameters_mms[idx][0] +
+                  '-' + parameters_mms[idx][1] + '-' +
+                  str(parameters_mms[idx][2][1][0]) + '-' +
+                  str(parameters_mms[idx][2][2]) + ' ' + '%.4f' % float(m))
+
+        imq_idms.append(imq_idm)
+        mms_idms.append(mms_idm)
+    
+    print('------------------------------------------------')
+    print('IMQ-rsFC')
+        
+    for i, r in enumerate(results_rsfc):
+        
+        r = [(j[0], [-1]) if j[0].split('-')[0] != target else j for j in r]
+
+        idx, m = sorted([p for p in enumerate(mean(r, num_p_rsfc))],
+                        key=lambda x: x[-1],
+                        reverse=True)[0]
+            
+        x = combination_rsfc[i][0]
+            
+        if parameters_rsfc[idx][0] in DSF_X_mvs:
+            x = combination_rsfc[i][0][0]
+                
+        if parameters_rsfc[idx][0] in DSF_X_uvs:
+            x = combination_rsfc[i][0][1]
+            
+        rho, _, rsfc_idm = run(x,
+                               combination_rsfc[i][1],
+                               parameters_rsfc[idx][0],
+                               parameters_rsfc[idx][1],
+                               idm=True)
+
+        print(titles[i] + ': ' + '%.4f' % float(rho) + ' achieved by ' + parameters_rsfc[idx][0] +
+              '-' + parameters_rsfc[idx][1] + ' ' + '%.4f' % float(m))
+
+        rsfc_idms.append(rsfc_idm)
+    
+    subject_left = [i+1 for i in range(24) for j in range(24) if i < j]+[j+1 for i in range(24) for j in range(24) if i < j]
+    subject_right = [j+1 for i in range(24) for j in range(24) if i < j]+[i+1 for i in range(24) for j in range(24) if i < j]
+    
+    for i, t in enumerate(titles):
+    
+        df = DataFrame({
+            t[:2]: list(zscore(imq_idms[i], nan_policy='omit')*-1)+list(zscore(imq_idms[i], nan_policy='omit')*-1),
+            t[-2:]+'_MMS': list(zscore(mms_idms[i])*-1)+list(zscore(mms_idms[i])*-1),
+            t[-2:]+'_rsFC': list(zscore(rsfc_idms[i])*-1)+list(zscore(rsfc_idms[i])*-1),
+            'Subject_left': subject_left,
+            'Subject_right': subject_right,
+        })
+        df.to_csv('./data/dra/'+t+'/'+target+'.csv')
+
+
+def regression_model(dv, iv):
+    
+    """
+    Construct all possible regerssion models (i.e., iterate all the distance metrics)
+
+    Parameters
+    ----------
+    dv: string 'SS' or 'SO' or 'OS'.
+        'SS' for self-self mentalisation, 'SO' for self-other mentalisation, 'OS' for other-self mentalisation.
+    iv: string 'RH' or 'LA' or 'RA'.
+        'RH' for right hippocampus, 'LA' for left amygdala, 'RA' for right amygdala.
+    """
+
+    for dsf in DSF_X_mvs + DSF_X_uvs:
+
+        data = pd.read_csv('./data/dra/' + dv + '-' +
+                           iv + '/' + dsf + '.csv')
+        print(
+            '----------------------------------------------------------------------------------------------------'
+        )
+        print(dsf)
+        print(spearmanr(data[iv + "_MMS"], data[iv + "_rsFC"]))
+        try:
+            model = Lmer(dv + " ~ " + iv + "_MMS * " + iv +
+                         "_rsFC + (1|Subject_left) + (1|Subject_right)",
+                         data=data)
+            print(model.fit())
+        except:
+            pass
+
+
+def specific_model(dsf, dv, iv):
+    
+    """
+    Construct the optimal regression model, which has the highest log-likelihood
+    
+    Parameters
+    ----------
+    dsf: string 'abs' or 'mean' or 'min' or 'abs*mean' or 'max' or 'pearson' or 'euclidean' or 'mahalanobis' or 'cosine' or 'manhattan'.
+        The method to calculate mentalising similarity.
+    dv: string 'SS' or 'SO' or 'OS'.
+        'SS' for self-self mentalisation, 'SO' for self-other mentalisation, 'OS' for other-self mentalisation.
+    iv: string 'RH' or 'LA' or 'RA'.
+        'RH' for right hippocampus, 'LA' for left amygdala, 'RA' for right amygdala.
+    """
+
+    data = pd.read_csv('./data/dra/' + dv + '-' + iv +
+                       '/' + dsf + '.csv')
+
+    model = Lmer(dv + " ~ " + iv + "_MMS * " + iv +
+                 "_rsFC + (1|Subject_left) + (1|Subject_right)",
+                 data=data)
+
+    min_rsFC = np.quantile(data[iv + '_rsFC'], 0.25)
+    max_rsFC = np.quantile(data[iv + '_rsFC'], 0.75)
+
+    categorical_rsFC = []
+
+    for i in data[iv + '_rsFC'][:276]:
+
+        if np.abs(i - min_rsFC) < np.abs(i - max_rsFC):
+            categorical_rsFC.append('Q1')
+        else:
+            categorical_rsFC.append('Q3')
+
+    print(model.fit())     
+    
+    return model, list(data[iv + '_MMS'][:276]), list(data[iv + '_rsFC'][:276]), list(model.fits[:276]), categorical_rsFC
